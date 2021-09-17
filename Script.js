f402de8f4d63922091f95bde81f394bb4d13aa38
Angelica Cortez <angelica.cortez@hotmail.it>
2021-09-17 14:50:44 +0200

    fixed all requirements

---
+++ Script.js
@@ -0,0 +1,36 @@
+﻿function changeButton() {
+
+    let emailInput = document.getElementById("email").value;
+    let button = document.getElementById("subscribeON");
+
+    if (emailInput != "") {
+        button.disabled = "";
+    }
+}
+
+function Subscribing() {
+    let email = document.getElementById("email").value;
+
+    if (email != "") {
+        localStorage.setItem('Email', email);
+        document.getElementById("myForm").style.display = "none";
+        document.getElementById("subscribeOFF").style.display = "";
+    }
+}
+
+function Unsubscribing() {
+    localStorage.clear();
+    document.getElementById("myForm").style.display = "";
+    document.getElementById("subscribeOFF").style.display = "none";
+    document.getElementById("subscriber").style.display = "none";
+}
+
+function checkSubscribed() {
+    let emailStored = localStorage.getItem('Email');
+
+    if (emailStored != null) {
+        document.getElementById("myForm").style.display = "none";
+        document.getElementById("subscriber").innerHTML = localStorage.getItem('Email');
+        document.getElementById("subscribeOFF").style.display = "";
+    }
+}
